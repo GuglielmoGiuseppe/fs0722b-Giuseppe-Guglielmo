2aed46db1779a565d456f720c99f0b3898c72602
GuglielmoGiuseppe <giuseppe.guglielmo96@gmail.com>
2022-12-07 16:49:47 +0100

    olaaa

---
+++ m1/w7/esercizi/d3 TodoList/system.js
@@ -0,0 +1,64 @@
+const FORM_STRING_VALUE = document.getElementById("input-list"); 
+const BUTTON = document.getElementById("button-add");
+const CONTAINER_LIST = document.querySelector("#container-list");
+var Index = 1;
+var ContainerSave = [];
+
+
+BUTTON.addEventListener("click",function(){
+    if(FORM_STRING_VALUE.value != "")
+        {
+
+             CreationElement(CONTAINER_LIST,FORM_STRING_VALUE.value);
+             ContainerSave.push(FORM_STRING_VALUE.value);
+             save(ContainerSave);
+             FORM_STRING_VALUE.value = "";
+             console.log(ContainerSave);
+        }
+        else{
+            return;
+    }
+});
+
+
+function CreationElement(conteiner, value)
+{
+    var createBox = document.createElement("div");
+    createBox.classList.add('Cell');
+    conteiner.append(createBox);
+    createBox.innerHTML = `\n<div><h3 class="m-1">${value}</h3></div>\n` + `\n<div class="me-2"><div class="button-x"></div></div>\n`;
+    const button =  createBox.querySelector(".button-x")
+    button.addEventListener("click", ()=>{
+        deleteData(value);
+    })
+    console.log(button);
+}
+
+//////////////
+////STORAGE SYSTEM
+/////////////////
+
+window.addEventListener("load", check);
+
+function save(value){
+    localStorage.setItem('Task', value);
+}
+function deleteData(value){
+    localStorage.removeItem("Task", value);
+}
+
+/** */
+function check(valueKey){
+    if(localStorage.getItem('Task',valueKey)){
+        console.log("ciao");
+         var list = localStorage.getItem('Task');
+         ContainerSave = JSON.parse(list);
+        console.log(ContainerSave);
+        for (let y = 0; y < ContainerSave.length; y++) {
+
+            CreationElement(CONTAINER_LIST,ContainerSave[y]);
+        }
+
+    }
+
+}
